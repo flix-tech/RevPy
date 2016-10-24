084c1128c4308d8d73cf10cda48ad5489d0fe4bf
Joerg <joerg.doepfert@flixbus.com>
2016-10-24 17:46:47 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='PyRM2',
+    version='0.1',
+    packages=['pyrm', 'pyrm.tests'],
+    license='',
+    long_description=open('README.md').read(),
+)
