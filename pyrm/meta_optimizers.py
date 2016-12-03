5c10def4347d0f4d713b7cd6d0207ae62543b576
Joerg <joerg.doepfert@flixbus.com>
2016-12-03 11:57:41 +0100

    added TODO for fare trafo decorator

--- pyrm/meta_optimizers.py
+++ pyrm/meta_optimizers.py
@@ -1,3 +1,5 @@
+from functools import wraps
+
 import numpy as np
 
 from pyrm.optimizers import calc_EMSRb
@@ -47,3 +49,35 @@ def calc_EMSRb_MR(fares, demands, sigmas=None, cap=None):
     return protection_levels
 
 
+# TODO: wrap_fare_trafo fails. fix that
+def wrap_fare_trafo(optimizer):
+
+    def wrapper(fares, demands, sigmas=None, cap=None):
+        if sigmas is None:
+            sigmas = np.zeros(fares.shape)
+
+        adjusted_fares, adjusted_demand = \
+            calc_fare_transformation(fares, demands, cap=cap)
+
+        # inefficient strategies correspond NaN adjusted fares
+        efficient_indices = np.where(~np.isnan(adjusted_fares))[0]
+
+        # calculate protection levels with EMSRb using efficient strategies
+        if adjusted_fares[efficient_indices].size:
+            protection_levels_temp = optimizer(adjusted_fares[efficient_indices],
+                                            adjusted_demand[efficient_indices],
+                                            sigmas[efficient_indices])
+            protection_levels = fill_nan(fares.shape, efficient_indices,
+                                         protection_levels_temp)
+        else:
+            # if there is no efficient strategy, return zeros as  protection
+            #  levels
+            protection_levels = np.zeros(fares.shape)
+
+        return protection_levels
+
+    return wrapper
+
+@wrap_fare_trafo
+def calc_EMSRb_MR2(fares, demands, sigmas=None, cap=None):
+    return calc_EMSRb(fares, demands, sigmas=None)
